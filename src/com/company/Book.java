cfe64aa8ece27f59fb904c26bbbd848863b4c138
bogoj <marijabozinovska1@gmail.com>
2023-02-07 22:55:01 +0100

    Revert "added dictionary names"
    This reverts commit a7a1268ef3da9a38388644d43871540ef2137f96.

--- src/com/company/Book.java
+++ src/com/company/Book.java
@@ -7,7 +7,6 @@ public class Book {
 
         LearnLanguage("Duden");
         useDictionery( "Duden2");
-        // TODO: check dictionery name 
     }
     public static void LearnLanguage (String name)
     {
